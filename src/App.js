bcb88e15a8591150b1f4d8ceccd791258276e859
onurataasar <onurataasar@gmail.com>
2022-02-13 01:38:38 +0300

    adding fade slides

--- src/App.js
+++ src/App.js
@@ -3,46 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import { Fade } from "react-slideshow-image";
 
-const slideImages = [
-  {
-    url: 'images/benetton.jpg',
-    caption: 'Slide 1'
-  },
-  {
-    url: 'images/happygirl.png',
-    caption: 'Slide 2'
-  },
-  {
-    url: 'images/people.png',
-    caption: 'Slide 3'
-  },
+const fadeImages = [
+  "images/benetton.jpg",
+  "images/happygirl.png",
+  "images/people.png"
 ];
-const properties = {
-  duration: 3000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  indicators: true,
-  responsive: [
-      {
-          breakpoint: 1024,
-          settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3
-          }
-      },
-      {
-          breakpoint: 500,
-          settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-          }
-      }
-  ]
-};
 
 function App() {
   return (
@@ -72,16 +40,18 @@ function App() {
         </div>
       </div>
       <div className="slide-container">
-        <Slide {...properties}>
-         {slideImages.map((slideImage, index)=> (
-            <div className="each-slide" key={index}>
-              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-                <span>{slideImage.caption}</span>
-              </div>
-            </div>
-          ))} 
-        </Slide>
-      </div>
+      <Fade >
+        <div className="each-fade">
+          <img src={fadeImages[0]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[1]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[2]} />
+        </div>
+      </Fade>
+    </div>
     </div>
   );
 }
