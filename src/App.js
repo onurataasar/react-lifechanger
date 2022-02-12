d0ad36fbc64243b77bee1033b77d61f1cc05c860
onurataasar <onurataasar@gmail.com>
2022-02-12 23:59:08 +0300

    login and signup continued

--- src/App.js
+++ src/App.js
@@ -3,12 +3,51 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import logo from "./components/logo.png";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+const slideImages = [
+  {
+    url: 'images/benetton.jpg',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'images/happygirl.png',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'images/people.png',
+    caption: 'Slide 3'
+  },
+];
+const properties = {
+  duration: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  indicators: true,
+  responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+          }
+      },
+      {
+          breakpoint: 500,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/sign-in"}>LifeChanger</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -26,14 +65,22 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Routes>
-            <Route exact path='/' element={<Login/>} />
-            <Route path="/sign-in" element={<Login/>} />
-            <Route path="/sign-up" element={<Signup/>} />
+            <Route exact path='/' element={<Login />} />
+            <Route path="/sign-in" element={<Login />} />
+            <Route path="/sign-up" element={<Signup />} />
           </Routes>
         </div>
       </div>
-      <div className="logo">
-          <img src={logo} alt="Logo" /> 
+      <div className="slide-container">
+        <Slide {...properties}>
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
       </div>
     </div>
   );
